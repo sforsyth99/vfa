8ea5ade95f87de1f70fbd6879f878c81bb1bff6b
sforsyth99 <sandra.forsyth@atono.io>
2026-04-03 08:42:06 -0700

    infinite rest call

--- src/api/posts/useGetPosts.ts
+++ src/api/posts/useGetPosts.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import wretch from '../wretch.ts';
 import { type Post, POSTS_PATH, POSTS_URL } from './postTypes.ts';
 
@@ -9,6 +9,36 @@ export function useGetPosts() {
       return wretch(`${POSTS_URL}`)
         .get()
         .json();
+    },
+    refetchOnWindowFocus: false,
+  });
+}
+
+export function useGetPaginatedPosts(page: number = 1, per_page: number = 10) {
+  return useQuery<Post[]>({
+    queryKey: [POSTS_PATH, page, per_page],
+    queryFn: async () => {
+      return wretch(`${POSTS_URL}?page=${page}&per_page=${per_page}`)
+        .get()
+        .json();
+    },
+    refetchOnWindowFocus: false,
+  });
+}
+
+export function useInfinitePosts(per_page: number = 10) {
+  return useInfiniteQuery<Post[], Error>({
+    queryKey: [POSTS_PATH, 'infinite', per_page],
+    queryFn: async ({ pageParam = 1 }) => {
+      return wretch(`${POSTS_URL}?page=${pageParam}&per_page=${per_page}`)
+        .get()
+        .json();
+    },
+    initialPageParam: 1,
+    getNextPageParam: (lastPage: Post[], allPages) => {
+      // If the last page returned less than per_page, there are no more pages
+      if (lastPage.length < per_page) return undefined;
+      return allPages.length + 1;
     },
     refetchOnWindowFocus: false,
   });
