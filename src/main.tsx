23125d3f8312a90dca18f15ccda7710636e761e1
sforsyth99 <sandra.forsyth@atono.io>
2026-07-21 21:00:50 -0700

    css vars

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { IntlProvider } from 'react-intl';
 import messages from './locales/en.json';
+import './styles/default.css';
 import './styles/global.css';
 
 import { AppProviders } from './AppProviders';
